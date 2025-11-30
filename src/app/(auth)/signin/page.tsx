638a40a512ad939dd56d0256d73eb0e6a8fe602d
ImadAlshikh <iimadgameover@gmail.com>
2025-11-30 22:20:09 +0300

    add signin page and login/signin buttons on header

--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FcGoogle } from "react-icons/fc";
 
 export default function page() {
@@ -8,10 +9,20 @@ export default function page() {
 
         <form className="login-form flex flex-col justify-center items-center md:flex-1 bg-white border border-border p-4  m-10 rounded-md">
           <div className="flex flex-col items-center">
-            <h2 className="text-primary font-bold text-2xl">Login</h2>
-            <div className="text-lg">Welcome back!</div>
+            <h2 className="text-primary font-bold text-2xl">Signin</h2>
+            <div className="text-lg">Welcome — let’s sign you in</div>
           </div>
           <div className="flex flex-col gap-4 w-90">
+            <div className="flex flex-col">
+              <label htmlFor="email">Name</label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Name"
+                className="px-2 py-2 ml-1 outline-text border border-border rounded-md focus:outline-2"
+              />
+            </div>
             <div className="flex flex-col">
               <label htmlFor="email">Email</label>
               <input
@@ -34,15 +45,16 @@ export default function page() {
             </div>
             <button
               type="button"
-              className="bg-primary hover:bg-primary-dark text-white rounded-md px-2 py-2"
+              className="bg-primary hover:bg-primary-dark text-white rounded-md px-2 py-2 flex items-center"
             >
-              Login
+              <span className="text-white grow">Signin</span>
+              <img src="/right-arrow.svg" alt="" className="w-[10%]" />
             </button>
             <a
-              href="/signin"
+              href="/login"
               className="text-center text-sm text-primary hover:text-primary-dark"
             >
-              you haven't account yet? signin
+              you already have a account? login
             </a>
             <div className="flex items-center gap-0.5">
               <hr className="bg-text flex-1" />
@@ -51,10 +63,13 @@ export default function page() {
             </div>
             <button
               type="button"
+              onClick={() =>
+                (window.location.href = "http://localhost:3001/auth/google")
+              }
               className="rounded-md border hover:bg-bg border-border p-2 flex items-center"
             >
               <FcGoogle size={24} />
-              <span className="grow">Login With Google</span>
+              <span className="grow">Signin With Google</span>
             </button>
           </div>
         </form>
--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -8,6 +8,7 @@ import Link from "next/link";
 
 export default function Header() {
   const [showMenu, setShowMenu] = useState<boolean>(false);
+  const [authed, setAuthed] = useState<boolean>(false);
   return (
     <header className="w-full bg-white overflow-x-clip relative border-b p-2 text-text border-border">
       <div className="header-container container mx-auto flex justify-between items-center ">
@@ -16,7 +17,26 @@ export default function Header() {
         </Link>
         <div className="flex items-center gap-1">
           <SearchBar />
-          <UserProfile />
+          {authed ? (
+            <UserProfile />
+          ) : (
+            <div className="flex items-center gap-0">
+              <button
+                type="button"
+                className="bg-primary hover:bg-primary-dark text-white rounded-full px-3 py-1 font-bold flex-1"
+              >
+                Login
+              </button>
+              <span className="text-3xl">/</span>
+              <button
+                type="button"
+                className="bg-white hover:bg-bg border border-primary text-primary font-bold flex-1 rounded-full px-3 py-1 "
+              >
+                Signin
+              </button>
+            </div>
+          )}
+
           <button type="button" onClick={() => setShowMenu((prev) => !prev)}>
             <CiMenuBurger className="menu-button md:hidden" size={24} />
           </button>
